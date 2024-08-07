337670241e36e6882581cb2b312cbd7972a09580
aseelshaheen <aseelshaheen080@gmail.com>
2024-08-07 22:20:02 +0300

    commit

--- healthDetails.js
+++ healthDetails.js
@@ -1,149 +1,21 @@
-function openBMIDialog() {
-    const dialog = document.getElementById('BMIDialog');
+const dialogIds = [
+    'BMIDialog', 'BtrDialog', 'TempDialog', 'SleepDialog',
+    'BloodDialog', 'HeartRDialog', 'WeightDialog', 'FatPDialog',
+    'AVGDialog', 'WorkoutDialog', 'HydrationDialog', 'msgDialog'
+];
+
+function openDialog(id) {
+    const dialog = document.getElementById(id);
     dialog.removeAttribute('hidden'); // Remove the hidden attribute
     dialog.showModal();
 }
 
-function closeBMIDialog() {
-    const dialog = document.getElementById('BMIDialog');
+function closeDialog(id) {
+    const dialog = document.getElementById(id);
     dialog.close();
     dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
 }
 
-
-function openBtrDialog() {
-    const dialog = document.getElementById('BtrDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeBtrDialog() {
-    const dialog = document.getElementById('BtrDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-function openTempDialog() {
-    const dialog = document.getElementById('TempDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeTempDialog() {
-    const dialog = document.getElementById('TempDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openSleepDialog() {
-    const dialog = document.getElementById('SleepDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeSleepDialog() {
-    const dialog = document.getElementById('SleepDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openBloodDialog() {
-    const dialog = document.getElementById('BloodDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeBloodDialog() {
-    const dialog = document.getElementById('BloodDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openHeartRDialog() {
-    const dialog = document.getElementById('HeartRDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeHeartRDialog() {
-    const dialog = document.getElementById('HeartRDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openWeightDialog() {
-    const dialog = document.getElementById('WeightDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeWeightDialog() {
-    const dialog = document.getElementById('WeightDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openFatPDialog() {
-    const dialog = document.getElementById('FatPDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeFatPDialog() {
-    const dialog = document.getElementById('FatPDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openAVGDialog() {
-    const dialog = document.getElementById('AVGDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeAVGDialog() {
-    const dialog = document.getElementById('AVGDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openWorkoutDialog() {
-    const dialog = document.getElementById('WorkoutDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeWorkoutDialog() {
-    const dialog = document.getElementById('WorkoutDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-function openHydrationDialog() {
-    const dialog = document.getElementById('HydrationDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
-
-function closeHydrationDialog() {
-    const dialog = document.getElementById('HydrationDialog');
-    dialog.close();
-    dialog.setAttribute('hidden', 'true'); // Re-add the hidden attribute
-}
-
-
-function openMsgDialog() {
-    const dialog = document.getElementById('msgDialog');
-    dialog.removeAttribute('hidden'); // Remove the hidden attribute
-    dialog.showModal();
-}
 
 // function closeMsgDialog() {
 //     const dialog = document.getElementById('msgDialog');
